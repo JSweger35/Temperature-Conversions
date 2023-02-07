438051ee77269977ee7cadf8d34222f21b2ec0b5
jsweger <jsweg13@gmail.com>
2023-02-07 08:54:56 -0500

    Add Temperature constructers and rearrange method locations

--- Temperature.java
+++ Temperature.java
@@ -12,6 +12,38 @@ public class Temperature
     private double degreesKelvin; // Kelvin temperature
 
     /**
+     * Default constructor for Temperature sets the default
+     * value of the Temperature object to 0.0 degrees
+     * Fahrenheit with Celsius and Kelvin equivalents
+     */
+    public Temperature() {
+        this(0.0);
+    }
+    
+    /**
+     * This constructor for Temperature sets the Fahrenheit
+     * value to the value from degrees, then calculates
+     * equivalent Celsius and Kelvin values using the method
+     * setDegreesFahrenheit()
+     * 
+     * @param degrees degrees Fahrenheit
+     */
+    public Temperature(double degrees) {
+        setDegreesFahrenheit(degrees);
+    }
+
+    /**
+     * The setDegreesFahrenheit method sets the Fahrenheit temperature
+     * 
+     * @param degrees The Fahrenheit value to store 
+     */
+    public void setDegreesFahrenheit(double degrees) {
+        degreesFahrenheit = degrees; // set Fahrenheit value
+        degreesCelsius = (degreesFahrenheit - 32.0) * 5.0 / 9.0; // set Celsius
+        degreesKelvin = degreesCelsius + 273.15; // set Kelvin value
+    }
+    
+    /**
      * getDegreesCelsius retrieves the Celsius temperature value
      * 
      * @return a double value containing the Celsius temperature
@@ -27,17 +59,6 @@ public class Temperature
      */
     public double getDegreesKelvin() {
         return degreesKelvin;
-    }
-
-    /**
-     * The setDegreesFahrenheit method sets the Fahrenheit temperature
-     * 
-     * @param degrees The Fahrenheit value to store 
-     */
-    public void setDegreesFahrenheit(double degrees) {
-        degreesFahrenheit = degrees; // set Fahrenheit value
-        degreesCelsius = (degreesFahrenheit - 32.0) * 5.0 / 9.0; // set Celsius
-        degreesKelvin = degreesCelsius + 273.15; // set Kelvin value
     }
 }
 
