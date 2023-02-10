48654240d55821328eea1c905e49b2fdad1dcde3
jsweger <jsweg13@gmail.com>
2023-02-10 09:18:31 -0500

    Add condition to temp type

--- TemperatureDriver.java
+++ TemperatureDriver.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 /**
-* TemperatureDriver runs and tests the Temperature class.
-* 
-* @author Jake Sweger
-* @version 2.0
-*/
+ * TemperatureDriver runs and tests the Temperature class.
+ * 
+ * @author Jake Sweger
+ * @version 2.0
+ */
 public class TemperatureDriver {
     /**
      * main() reads Fahrenheit temperatures and 
@@ -14,19 +14,24 @@ public class TemperatureDriver {
         double inputTemperature = 0.0;
         Scanner keyboard = new Scanner(System.in); 
         Temperature t1 = new Temperature();
-        String temperaturetype= "";
-        
+        String temperatureType= "";
+
         System.out.print("Enter a temperature type (C for Celsius, " +
-        "F for Fahrenheit, K for Kelvin): ");
-        System.out.print("Enter a Fahrenheit temperature: "); 
+            "F for Fahrenheit, K for Kelvin): ");
+        temperatureType = keyboard.next();
+        System.out.print("Enter a temperature: "); 
         inputTemperature = keyboard.nextDouble(); 
-        System.out.println("You entered " + inputTemperature + 
+
+        if (temperatureType.equals("f")){
+            System.out.println("You entered " + inputTemperature + 
                 " degrees Fahrenheit"); 
-        t1.setDegreesFahrenheit(inputTemperature);
-        System.out.println("which is " + t1.getDegreesCelsius() + 
+
+            t1.setDegreesFahrenheit(inputTemperature);
+            System.out.println("which is " + t1.getDegreesCelsius() + 
                 " degrees Celsius"); 
-        System.out.println("and " + t1.getDegreesKelvin() + 
+            System.out.println("and " + t1.getDegreesKelvin() + 
                 " degrees Kelvin.");
-        System.out.print("Enter another Fahrenheit temperature: ");
+            System.out.print("Enter another Fahrenheit temperature: ");
+        }
     }
 }
